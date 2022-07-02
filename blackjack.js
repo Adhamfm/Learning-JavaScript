7ee31a191ae41b71d49b41cfabed3644e0ef3228
Adhamfm <57398840+Adhamfm@users.noreply.github.com>
2022-07-02 13:02:01 +0200

    added player object

--- blackjack.js
+++ blackjack.js
@@ -11,6 +11,14 @@ let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardEl = document.getElementById("card-el")
 
+let player = {
+    name: "Per",
+    chips: 145
+}
+
+let playerEl = document.getElementById("player-el")
+playerEl.textContent = player.name + ": $" + player.chips
+
 function getRandomNumber() {
     let number = Math.floor(Math.random() * 13) + 1
     if (number === 1) {
